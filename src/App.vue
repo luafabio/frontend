3624acdbfc7e69a7e75d6712c802a96e817b5d5a
Luis Anibal Fabio <luis.fabio@mercadolibre.com>
2019-06-03 21:18:19 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    TEST
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'App',
+    components: {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>
