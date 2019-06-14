e03e02f10c0f0f7a6eb4b26ccd7285c0137066c3
Luis Anibal Fabio <luis.fabio@mercadolibre.com>
2019-06-14 04:53:17 -0300

    cambios en los mapas

--- src/components/BusDetail.vue
+++ src/components/BusDetail.vue
@@ -12,13 +12,16 @@
         mapTypeControl: false,
       }"
       style="height: 500px">
+
       <GmapMarker
         v-bind:key="index"
         v-for="(m, index) in markersBus"
         v-bind:position="m.position"
         v-bind:clickable="true"
-        icon="http://maps.google.com/mapfiles/kml/paddle/go.png"
+        :icon="{ url: require('../../assets/marker-f.png'),}"
         />
+
+      <gmap-polyline v-bind:path.sync="path" v-bind:options="{ strokeColor:'#009BFF'}"></gmap-polyline>
     </GmapMap>
   </div>
 </template>
@@ -41,14 +44,47 @@
           lng: -59.037281
         },
         bus: [],
+        stops: [],
         markersBus: [],
-        markersStop: [],
+        path:  [
+          {lat: -34.097403,lng: -59.037281},
+          {lat: -34.097429,lng: -59.036644},
+          {lat: -34.100465,lng: -59.034390},
+          {lat: -34.101389,lng: -59.033792},
+          {lat: -34.107586,lng: -59.029163},
+          {lat: -34.105041,lng: -59.024299},
+          {lat: -34.104538,lng: -59.023421},
+          {lat: -34.105858,lng: -59.022368},
+          {lat: -34.107714,lng: -59.021291},
+          {lat: -34.112829,lng: -59.020346},
+          {lat: -34.115419,lng: -59.020215},
+          {lat: -34.118328,lng: -59.019672},
+          {lat: -34.149021,lng: -59.002130},
+          {lat: -34.151400,lng: -58.999394},
+          {lat: -34.154671,lng: -58.994520},
+          {lat: -34.157472,lng: -58.989328},
+          {lat: -34.160375,lng: -58.983693},
+          {lat:-34.162363,lng: -58.979895},
+          {lat: -34.162471,lng: -58.977402},
+          {lat: -34.162404,lng: -58.974103},
+          {lat: -34.16246,lng: -58.972394},
+          {lat: -34.162548,lng: -58.969455},
+          {lat: -34.162574,lng: -58.969337},
+          {lat: -34.163551,lng: -58.969456},
+          {lat: -34.163789,lng: -58.966078},
+          {lat: -34.163923,lng: -58.963729},
+          {lat: -34.164102,lng: -58.961538},
+          {lat: -34.164129,lng: -58.960463},
+          {lat: -34.165096,lng: -58.960497},
+          {lat: -34.169738,lng: -58.960964},
+          {lat: -34.171686,lng: -58.961181},
+          {lat: -34.175358,lng: -58.961496},
+          {lat: -34.176905,lng: -58.961752},
+        ],
       }
     },
     methods: {
       fetchBus(id) {
-        axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
-        axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
         axios.get(`${BASE_URL}list-buses/` + id)
           .then(resp => {
             this.bus = JSON.parse(JSON.stringify(resp.data));
--- src/components/Stops.vue
+++ src/components/Stops.vue
@@ -1,72 +1,79 @@
 <template>
   <div class="stops container">
     <Alert v-if="alert" v-bind:message="alert"/>
-      <router-link class="btn btn-secondary float-right" style="margin-top: 10px" to="/add">Agregar Parada</router-link>
+    <router-link class="btn btn-secondary float-right" style="margin-top: 10px" to="/add">Agregar Parada</router-link>
 
     <h1 class="page-header">Paradas de Colectivos</h1>
     <div class="panel-group" id="accordion">
       <div class="panel panel-default">
         <div class="panel-heading">
           <h4 class="panel-title">
-            <hr width="75%" />
-            <button class="btn btn-secondary btn-lg btn-block" type="button" data-toggle="collapse" data-target="#map" aria-expanded="false" aria-controls="collapseExample">
+            <hr width="75%"/>
+            <button class="btn btn-secondary btn-lg btn-block" type="button" data-toggle="collapse" data-target="#map"
+                    aria-expanded="false" aria-controls="collapseExample">
               Ver mapa
             </button>
           </h4>
         </div>
         <div id="map" class="collapse ">
-          <GmapMap class="my-20 borde"
-            v-bind:center="center"
-            v-bind:zoom="15"
-            :options="{
-              zoomControl: false,
-              disableDefaultUi: true,
-              streetViewControl: false,
-              mapTypeControl: false
-            }"
-            style="height: 500px">
+          <GmapMap class="my-20 borde" v-bind:center="center" v-bind:zoom="13" :options="{ zoomControl: false,  disableDefaultUi: true, streetViewControl: false, mapTypeControl: false }"
+                   style="height: 500px">
             <GmapMarker
               v-bind:key="index"
               v-for="(m, index) in markersStop"
               v-bind:position="m.position"
-              v-bind:clickable="true"/>
+              v-bind:clickable="true"
+              @click="openInfoWindowTemplate(index)"
+              :icon="{ url: require('../../assets/marker-b.png'),}"
+            />
+
+            <gmap-info-window
+              :options="{maxWidth: 300}"
+              :position="infoWindow.position"
+              :opened="infoWindow.open"
+              @closeclick="infoWindow.open=false">
+              <div v-html="infoWindow.template"></div></gmap-info-window>
+
+            <gmap-polyline v-bind:path.sync="path" v-bind:options="{ strokeColor:'#009BFF'}"></gmap-polyline>
+
           </GmapMap>
+
         </div>
       </div>
     </div>
-    <hr width="75%" />
+    <hr width="75%"/>
     <div class="form-group">
-      <input class="form-control align-content-center justify-content-center" placeholder="Ingrese el nombre de la parada de colectivo"
+      <input class="form-control align-content-center justify-content-center"
+             placeholder="Ingrese el nombre de la parada de colectivo"
              v-model="filterInput">
     </div>
-      <br>
-      <table class="table table-striped">
-        <thead>
-        <tr>
-          <th scope="col">N. Parada</th>
-          <th scope="col">Latitud</th>
-          <th scope="col">Longitud</th>
-          <th scope="col">Estado</th>
-          <th scope="col">Nombre</th>
-          <th></th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="stop in filterBy(stops, filterInput)">
-          <td>{{stop.num_stop}}</td>
-
-          <td>{{stop.lat}}</td>
-          <td>{{stop.long}}</td>
-          <td v-if="stop.status == true">Activa</td>
-          <td v-else>No Activa</td>
-          <td>{{stop.name}}</td>
-          <td>
-            <router-link class="btn btn-secondary" v-bind:to="'/stop/'+stop._id">Ver</router-link>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
+    <br>
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th scope="col">N. Parada</th>
+        <th scope="col">Latitud</th>
+        <th scope="col">Longitud</th>
+        <th scope="col">Estado</th>
+        <th scope="col">Nombre</th>
+        <th></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="stop in filterBy(stops, filterInput)">
+        <td>{{stop.num_stop}}</td>
+        <td>{{stop.lat}}</td>
+        <td>{{stop.long}}</td>
+        <td v-if="stop.status == true">Activa</td>
+        <td v-else>No Activa</td>
+        <td>{{stop.name}}</td>
+        <td>
+          <router-link class="btn btn-secondary" v-bind:to="'/stop/'+stop._id">Ver</router-link>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -87,14 +94,59 @@
           lat: -34.097403,
           lng: -59.037281
         },
+        infoWindow: {
+          position: {lat: 0, lng: 0},
+          open: false,
+          template: ''
+        },
+        path:  [
+            {lat: -34.097403,lng: -59.037281},
+            {lat: -34.097429,lng: -59.036644},
+            {lat: -34.100465,lng: -59.034390},
+            {lat: -34.101389,lng: -59.033792},
+            {lat: -34.107586,lng: -59.029163},
+            {lat: -34.105041,lng: -59.024299},
+            {lat: -34.104538,lng: -59.023421},
+            {lat: -34.105858,lng: -59.022368},
+            {lat: -34.107714,lng: -59.021291},
+            {lat: -34.112829,lng: -59.020346},
+            {lat: -34.115419,lng: -59.020215},
+            {lat: -34.118328,lng: -59.019672},
+            {lat: -34.149021,lng: -59.002130},
+            {lat: -34.151400,lng: -58.999394},
+            {lat: -34.154671,lng: -58.994520},
+            {lat: -34.157472,lng: -58.989328},
+            {lat: -34.160375,lng: -58.983693},
+            {lat:-34.162363,lng: -58.979895},
+            {lat: -34.162471,lng: -58.977402},
+            {lat: -34.162404,lng: -58.974103},
+            {lat: -34.16246,lng: -58.972394},
+            {lat: -34.162548,lng: -58.969455},
+            {lat: -34.162574,lng: -58.969337},
+            {lat: -34.163551,lng: -58.969456},
+            {lat: -34.163789,lng: -58.966078},
+            {lat: -34.163923,lng: -58.963729},
+            {lat: -34.164102,lng: -58.961538},
+            {lat: -34.164129,lng: -58.960463},
+            {lat: -34.165096,lng: -58.960497},
+            {lat: -34.169738,lng: -58.960964},
+            {lat: -34.171686,lng: -58.961181},
+            {lat: -34.175358,lng: -58.961496},
+            {lat: -34.176905,lng: -58.961752},
+          ],
         markersStop: [],
         onceStops: 0,
         stops: [],
         alert: '',
-        filterInput: ''
+        filterInput: '',
       }
     },
     methods: {
+      openInfoWindowTemplate(index) {
+        this.infoWindow.template = `${this.stops[index].num_stop} - ${this.stops[index].name}`;
+        this.infoWindow.position = this.markersStop[index].position;
+        this.infoWindow.open = true
+      },
       fetchStops() {
         axios.get(`${BASE_URL}stops`)
           .then(resp => {
